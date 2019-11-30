2ac84ccdd1b642fa4c4c4d01da94d2f657af7659
Akhil Bagaria <abagaria@hmc.edu>
2019-11-30 15:23:07 -0500

    Changes to SAC launch script

--- rlpyt/experiments/scripts/mujoco/qpg/launch/launch_mujoco_sac_serial.py
+++ rlpyt/experiments/scripts/mujoco/qpg/launch/launch_mujoco_sac_serial.py
@@ -1,4 +1,4 @@
-
+from random import randint
 from rlpyt.utils.launching.affinity import encode_affinity
 from rlpyt.utils.launching.exp_launcher import run_experiments
 from rlpyt.utils.launching.variant import make_variants, VariantLevel
@@ -6,19 +6,19 @@ from rlpyt.utils.launching.variant import make_variants, VariantLevel
 script = "rlpyt/experiments/scripts/mujoco/qpg/train/mujoco_sac_serial.py"
 affinity_code = encode_affinity(
     n_cpu_core=2,
-    n_gpu=0,
-    hyperthread_offset=2,
-    n_socket=1,
-    cpu_per_run=1,
+    n_gpu=1,
+    # hyperthread_offset=2,
+    # n_socket=1,
+    # cpu_per_run=1,
 )
 runs_per_setting = 2
 default_config_key = "sac_1M_serial"
 experiment_title = "sac_mujoco"
 variant_levels = list()
 
-env_ids = ["Hopper-v2"]  # , "Swimmer-v3"]
+env_ids = ["Ant-v2"]  # , "Swimmer-v3"]
 values = list(zip(env_ids))
-dir_names = ["env_{}".format(*v) for v in values]
+dir_names = ["env_{}_{}".format(*v, randint(0, 1000)) for v in values]
 keys = [("env", "id")]
 variant_levels.append(VariantLevel(keys, values, dir_names))
 
